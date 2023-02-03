05dddcf599bdede2328d84be0801f84b8ee20f9b
Vishwas <hvvishwas6@gmail.com>
2023-02-03 17:04:10 +0530

    first problem

---
+++ Basics/areaRect.js
@@ -0,0 +1,19 @@
+/* 
+Program to calculate the area of rectangle
+Input:
+Width = 10, Height = 5  
+Output:
+Area of Rectangle = Width * Height
+                  = 10 * 5
+                  = 50
+
+algorithm: 
+Step1: define and initilize width, height
+Step2: create variable named area and calculate area and calculate area by w*h
+Step3: Print area 
+*/
+
+let width= 10
+let height= 5
+let area= width*height
+console.log(area)
