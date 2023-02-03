44b38b9f82afef08ed00f92b6867bbee1a0ef522
Vishwas <hvvishwas6@gmail.com>
2023-02-03 17:08:39 +0530

    first problem

---
+++ Basics/areaRect.py
@@ -0,0 +1,17 @@
+"""
+Program to calculate the area of rectangle
+Input:
+Width = 10, Height = 5  
+Output:
+Area of Rectangle = Width * Height
+                  = 10 * 5
+                  = 50
+algorithm: 
+Step1: define and initilize width, height
+Step2: create variable named area and calculate area and calculate area by w*h
+Step3: Print area              
+"""
+width=10 
+height=5
+area=width*height
+print(area)
